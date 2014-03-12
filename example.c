0a16f0e7bbb642606055338f6c3718ec8dd04e1a
Stephen Mathieson <smath23@gmail.com>
2014-03-12 14:00:55 -0400

    refactor example

--- example.c
+++ example.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 #include "clib-package.h"
 
-#define true 1
-
 int main(int argc, char const *argv[]) {
   for (int i = 1; i < argc; ++i) {
     clib_package_t *pkg = clib_package_new_from_slug(argv[i], 1);
     if (!pkg) return 1;
-    int rc = clib_package_install(pkg, "./deps", true);
+    int rc = clib_package_install(pkg, "./deps", 1);
     clib_package_free(pkg);
     if (0 != rc) return 2;
   }
